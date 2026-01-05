6adcd4b049ce5edb4c62a09d022654d7e8e93a9e
BrainADZ <web@brainadz.marketing>
2026-01-05 13:59:16 +0530

    chamnges gitignore

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "Meesho Clone",
-  description: "Multi-vendor ecommerce platform built with Next.js",
+  title: "Country Home india best E-commerce Platform",
+  description: "Country Home india best E-commerce Platform",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
